d3d10ec57e21a22232421fe21458dbe698389005
tas0dev <156896971+tas0dev@users.noreply.github.com>
2026-06-26 14:41:27 +0900

    fix

--- usb-driver/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR missing");
-    println!("cargo:rustc-link-arg=-T{}/linker.ld", manifest_dir);
-    println!("cargo:rerun-if-changed={}/linker.ld", manifest_dir);
-}
